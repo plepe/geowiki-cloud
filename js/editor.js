866df6469cdb6e7ef5c023879a586c866fbfae96
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-05-27 19:17:09 +0200

    Show a header with filename, save and close button

--- js/editor.js
+++ js/editor.js
@@ -11,12 +11,35 @@ module.exports = {
 
   _onEditorTrigger (filename, context) {
     let parentDom = document.getElementById('content')
+
     let dom = document.createElement('div')
     dom.id = 'geoedit'
     parentDom.style.position = 'relative'
     parentDom.appendChild(dom)
 
-    let editor = new Editor({ dom })
+    let controls = document.createElement('div')
+    controls.className = 'editor_controls'
+    dom.appendChild(controls)
+
+    let x = document.createElement('small')
+    x.className = 'filename'
+    x.appendChild(document.createTextNode(filename))
+    controls.appendChild(x)
+
+    x = document.createElement('button')
+    x.className = 'icon-close svg'
+    controls.appendChild(x)
+    x.onclick = () => parentDom.removeChild(dom)
+
+    x = document.createElement('button')
+    x.appendChild(document.createTextNode('💾'))
+    controls.appendChild(x)
+
+    let mapDiv = document.createElement('div')
+    mapDiv.className = 'map'
+    dom.appendChild(mapDiv)
+
+    let editor = new Editor({ dom: mapDiv })
 
     this.loadFile(context.dir, filename, (err, filedata) => {
       if (err) {
